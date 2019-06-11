de1ef173abb9e302f86433b8ba7f19f5b1d2e311
Christopher Thomas <chris.thomas@hotmail.com.au>
2019-06-12 02:40:24 +1000

    Bug Fix, Changed size of image to stop characters being cut off, and change coordinates of where it gets placed within file

--- ImageMaker.py
+++ ImageMaker.py
@@ -8,11 +8,11 @@ fileLocation = "C:///Users/chris/Downloads/tng/ant.mkv"
 uniqueCode = "123abc"
 
 def watchVideo(dir, uniqueCode):
-    img = Image.new('RGBA', (200, 60))
+    img = Image.new('RGBA', (200, 80))
 
     fnt = ImageFont.truetype('C:///Windows/WinSxS/amd64_microsoft-windows-font-truetype-arial_31bf3856ad364e35_10.0.17134.1_none_5803fc87168579d6/Arial.ttf', 72)
     d = ImageDraw.Draw(img)
-    d.text((20,20), uniqueCode,font = fnt, fill=(255,255,0))
+    d.text((0,0), uniqueCode,font = fnt, fill=(255,255,0))
     print("test  ")
 
     img.save('C:///Users/Christopher/source/repos/RampSoftware/pil_text.png')
